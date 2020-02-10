17e3d4bcdcce7ba94bf9b3759b85944201a963ed
Oluwafemi Shobande <oluwafemi.shobande@topia.com>
2020-02-10 15:38:52 +0200

    I think ussdCode is clearer than serviceCode

--- src/main/kotlin/gateway/Request.kt
+++ src/main/kotlin/gateway/Request.kt
@@ -3,7 +3,7 @@ package gateway
 data class Request(
     val phoneNumber: String,
     val sessionId: String,
-    val serviceCode: String?,
+    val ussdCode: String?,
     val message: String,
     val operator: String?
 )
